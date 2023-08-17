66474e2210cd116294aaf4b3411d2765535c0e67
Winifred <46898257+winigalviz@users.noreply.github.com>
2023-08-17 16:37:32 -0500

    connect endpoint

--- src/disney/pages/ListarPeliculasPage.jsx
+++ src/disney/pages/ListarPeliculasPage.jsx
@@ -14,133 +14,111 @@ import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 import DisneyLayout from "../layout/DisneyLayout";
 import ButtonAdd from '../components/ButtonAdd';
 
-function createData(imagen, titulo, fecha_creacion) {
-  return {
-    imagen, 
-    titulo, 
-    fecha_creacion,
-    detalle: [
-      {
-        calificacion: '5',
-        personajes_asociados: '11091700',
-      },
-      {
-        calificacion: '10',
-        personajes_asociados: '11',
-      },
-    ],
-  };
-}
+import {table} from '../service/pelicuulasService';
+import {useEffect, useState} from "react";
 
 const Row = (props) => {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
+    const {row} = props;
+    const [open, setOpen] = React.useState(false);
 
-  return (
-    <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        <TableCell component="th" scope="row" align="center">{row.imagen}</TableCell>
-        <TableCell align="center">{row.titulo}</TableCell>
-        <TableCell align="center">{row.fecha_creacion}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                detalle
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="left">Calificaciones</TableCell>
-                    <TableCell align="right">Personajes_Asociados</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.detalle.map((historyRow) => (
-                    <TableRow key={historyRow.calificacion}>
-                      <TableCell align='left' component="th" scope="row">
-                        {historyRow.calificacion}
-                      </TableCell>
-                      <TableCell align='right'>{historyRow.personajes_asociados}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <TableRow sx={{'& > *': {borderBottom: 'unset'}}}>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row" align="center">{row.imagen}</TableCell>
+                <TableCell align="center">{row.titulo}</TableCell>
+                <TableCell align="center">{row.fechaCreacion}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{paddingBottom: 0, paddingTop: 0}} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{margin: 1}}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Personajes
+                            </Typography>
+                            <Table size="small" aria-label="purchases">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align="left">imagen</TableCell>
+                                        <TableCell align="right">nombre</TableCell>
+                                        <TableCell align="right">edad</TableCell>
+                                        <TableCell align="right">peso</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {row.Personajes.map((personaje) => (
+                                        <TableRow key={personaje.id}>
+                                            <TableCell align='left' component="th" scope="row">
+                                                {personaje.imagen}
+                                            </TableCell>
+                                            <TableCell align='right'>{personaje.nombre}</TableCell>
+                                            <TableCell align='right'>{personaje.edad}</TableCell>
+                                            <TableCell align='right'>{personaje.peso}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
 }
 
-Row.propTypes = {
-  row: PropTypes.shape({
-    imagen: PropTypes.string.isRequired,
-    titulo: PropTypes.string.isRequired,
-    fecha_creacion: PropTypes.string.isRequired,
-    detalle: PropTypes.arrayOf(
-      PropTypes.shape({
-        calificacion: PropTypes.string.isRequired,
-        Personajes_Asociados: PropTypes.string.isRequired,
-      }),
-    )
-  }).isRequired,
-};
-
-const rows = [
-  createData('imagenGoku', 'Gooku1', 'fecha_creacion'),
-  createData('imagenGoku', 'Gooku2', 'fecha_creacion'),
-  createData('imagenGoku', 'Gooku3', 'fecha_creacion'),
-  createData('imagenGoku', 'Gooku4', 'fecha_creacion'),
-  createData('imagenGoku', 'Gooku5', 'fecha_creacion'),
-];
-
 const ListarPeliculasPage = () => {
-  return (
-    <DisneyLayout>
-        <Typography variant='h3' align='center' p={1}>Listado de Peliculas o Series</Typography>
-        <TableContainer component={Paper}>
-            <Table aria-label="collapsible table">
-                <TableHead>
-                    <TableRow sx={{backgroundColor: 'blue'}}>
-                        <TableCell />
-                        <TableCell sx={{fontSize:'20px', color:'white', fontWeight:'bold'}} align="center">Imagen</TableCell>
-                        <TableCell sx={{fontSize:'20px', color:'white', fontWeight:'bold'}} align="center">Titulo</TableCell>
-                        <TableCell sx={{fontSize:'20px', color:'white', fontWeight:'bold'}} align="center">Fecha_Creacion</TableCell>
-                        <TableCell sx={{fontSize:'20px', color:'white', fontWeight:'bold'}} align="center">Acciones</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                {rows.map((row) => (
-                    <Row key={row.name} row={row} />
-                ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+    const [peliculas, setPeliculas] = useState([]);
 
-        <Link to="/disney/agregar-pelicula">
-            <ButtonAdd/>
-        </Link>
-    </DisneyLayout>
-    
-  );
+    useEffect(() => {
+        table()
+            .then(el => { setPeliculas(el.message.data) });
+    }, []);
+
+    return (
+        <DisneyLayout>
+            <Typography variant='h3' align='center' p={1}>Listado de Peliculas o Series</Typography>
+            <TableContainer component={Paper}>
+                <Table aria-label="collapsible table">
+                    <TableHead>
+                        <TableRow sx={{backgroundColor: 'blue'}}>
+                            <TableCell/>
+                            <TableCell sx={{fontSize: '20px', color: 'white', fontWeight: 'bold'}}
+                                       align="center">Imagen</TableCell>
+                            <TableCell sx={{fontSize: '20px', color: 'white', fontWeight: 'bold'}}
+                                       align="center">Titulo</TableCell>
+                            <TableCell sx={{fontSize: '20px', color: 'white', fontWeight: 'bold'}}
+                                       align="center">Fecha_Creacion</TableCell>
+                            <TableCell sx={{fontSize: '20px', color: 'white', fontWeight: 'bold'}}
+                                       align="center">Acciones</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {peliculas.map((row) => (
+                            <Row key={row.name} row={row}/>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <Link to="/disney/agregar-pelicula">
+                <ButtonAdd/>
+            </Link>
+        </DisneyLayout>
+
+    );
 }
 
 export default ListarPeliculasPage;
